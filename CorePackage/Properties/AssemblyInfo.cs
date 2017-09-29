0577f225b3f790194445f02b9ff21d412f6bb766
Fernand <fernand.veyrier@epitech.eu>
2017-09-30 01:58:20 +0200

    added diagrams

--- CorePackage/Properties/AssemblyInfo.cs
+++ CorePackage/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CorePackage")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Package for Duly main mecanisms.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Salty Studio")]
 [assembly: AssemblyProduct("CorePackage")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
