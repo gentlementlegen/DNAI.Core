0cd685c75349d15e2b5968ba5af1b6a9e8bceb1f
Quentin_Gasparotto <quentin.gasparotto@epitech.eu>
2017-10-04 19:46:00 -0400

    Changes on objects
    Changes on the attributes system in objects
    Adding unit test file on list and objects
    
    Objects and Lists will be developped in another feature

---
+++ CoreTest/TestEntities/TestListsObjects.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreTest.TestEntities
+{
+    [TestClass]
+    class TestLists
+    {
+        [TestMethod]
+        public void TestListActions()
+        {
+            CorePackage.Entity.Type.ListType type = new CorePackage.Entity.Type.ListType(CorePackage.Entity.Type.Scalar.Integer);
+
+            //append
+            //  input:
+            //      - value
+
+            //foreach
+            //  outputs:
+            //      - index
+            //      - value
+
+            //insert
+            //  inputs:
+            //      - index
+            //      - value
+
+            //remove
+            //  inputs:
+            //      - index
+
+            //clear
+            
+            //size
+            //  outputs:
+            //      - size
+        }
+
+        [TestMethod]
+        public void TestObjectActions()
+        {
+            CorePackage.Entity.Type.ObjectType type = new CorePackage.Entity.Type.ObjectType(null);
+
+            type.AddPublicAttribute("x", CorePackage.Entity.Type.Scalar.Integer);
+            type.AddPublicAttribute("y", CorePackage.Entity.Type.Scalar.Integer);
+            type.AddPublicAttribute("z", CorePackage.Entity.Type.Scalar.Integer);
+            
+            //show fields
+
+            //method call -> nouvel objet method qui hérite de function -> rename de l'instruction FunctionCall en Call
+        }
+    }
+}
