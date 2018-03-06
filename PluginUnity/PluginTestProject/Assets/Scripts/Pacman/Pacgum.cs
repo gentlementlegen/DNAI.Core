1a23fc402e34a41005db4d2d0e50d434449f8502
Fernand <fernand.veyrier@epitech.eu>
2018-03-06 17:13:35 +0100

    Adding sounds (WIP)

--- PluginUnity/PluginTestProject/Assets/Scripts/Pacman/Pacgum.cs
+++ PluginUnity/PluginTestProject/Assets/Scripts/Pacman/Pacgum.cs
@@ -5,7 +5,17 @@ namespace Assets.Scripts.Pacman
     public class Pacgum : MonoBehaviour
     {
         [SerializeField]
+        private AudioClip _eatSound;
+
+        [SerializeField]
         private int _reward = 100;
+
+        private AudioSource _audioSource;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
 
         private void OnTriggerStay2D(Collider2D other)
         {
@@ -13,6 +23,7 @@ namespace Assets.Scripts.Pacman
             {
                 GameManager.Instance.AddScore(_reward);
                 TerrainManager.Instance.OnGumEaten();
+                _audioSource.PlayOneShot(_eatSound);
                 Destroy(gameObject);
             }
         }
