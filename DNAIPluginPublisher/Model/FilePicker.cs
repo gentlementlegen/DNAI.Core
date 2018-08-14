3898a7851fa15aa34636d789a7f739a5eda56598
Fernand <fernand.veyrier@epitech.eu>
2018-08-14 22:40:08 +0200

    Design window for the DNAI Plugin Publisher

---
+++ DNAIPluginPublisher/Model/FilePicker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// NOTES
+// https://www.codeproject.com/Articles/167873/A-WPF-File-ListView-and-ComboBox
+// https://stackoverflow.com/questions/15104986/how-to-list-files-in-directory-c-sharp-wpf
+// https://stackoverflow.com/questions/6415037/populate-treeview-from-list-of-file-paths-in-wpf
+namespace DNAIPluginPublisher.Model
+{
+    class FilePicker
+    {
+    }
+}
