c46f200258f660e55628a8baf2309d51d438f34a
Fernand <fernand.veyrier@epitech.eu>
2018-08-14 22:40:08 +0200

    Design window for the DNAI Plugin Publisher

---
+++ DNAIPluginPublisher/Model/Logger.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Data;
+
+namespace DNAIPluginPublisher.Model
+{
+    public enum LEVEL { Info, Warning, Error }
+
+    /// <summary>
+    /// Represents a log.
+    /// </summary>
+    public class LogItem
+    {
+        public string Message { get; }
+
+        public DateTime Time { get; }
+
+        public LEVEL Level { get; }
+
+        public string MessageFormatted { get; }
+
+        public LogItem(string message) : this(message, LEVEL.Info)
+        {
+        }
+
+        public LogItem(string message, LEVEL level)
+        {
+            Message = message;
+            Level = level;
+            Time = DateTime.Now;
+            MessageFormatted = $"[{Time}] {Message}";
+        }
+    }
+
+    /// <summary>
+    /// Class that helps for logging messages into the console.
+    /// </summary>
+    public static class Logger
+    {
+        private static readonly ObservableCollection<LogItem> _logs = new ObservableCollection<LogItem>();
+        private static object _lock = new object();
+
+        public static IReadOnlyList<LogItem> Logs => _logs;
+
+        static Logger()
+        {
+            BindingOperations.EnableCollectionSynchronization(_logs, _lock);
+        }
+
+        public static void Log(string message)
+        {
+            _logs.Add(new LogItem(message));
+        }
+
+        public static void Log(string message, LEVEL level)
+        {
+            _logs.Add(new LogItem(message, level));
+        }
+
+        public static void Clear()
+        {
+            _logs.Clear();
+        }
+    }
+}
