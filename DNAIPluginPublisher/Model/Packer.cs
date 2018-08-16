7d0d0324a230ba23689aae8cd02bc16e91655408
Fernand <fernand.veyrier@epitech.eu>
2018-08-16 02:48:53 +0200

    Added item picker

---
+++ DNAIPluginPublisher/Model/Packer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace DNAIPluginPublisher.Model
+{
+    public class Packer
+    {
+        public void Pack(IEnumerable<Item> items)
+        {
+            Logger.Log("Packaging files");
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo
+            {
+                //WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+                FileName = "\"D:\\Program Files (x86)\\Unity\\Editor\\Unity.exe\"",
+                //Arguments = @"/c ""D:\Program Files (x86)\Unity\Editor\Unity.exe"" -quit -batchmode -projectPath ""something"" -exportPackage toto DNAI",
+                Arguments = "-quit -batchmode -projectPath=\"D:\\Folders\\VisualStudio\\Duly\\PluginUnity\\PluginTestProject\" -exportPackage \"Assets/Animations\" C:\\Users\\ferna\\Desktop\\PluginUploader\\TODELEEEE.unityPackage",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            process.StartInfo = startInfo;
+            System.Diagnostics.Debug.WriteLine(startInfo.Arguments);
+            process.Start();
+            process.WaitForExit();
+            if (process.ExitCode == 0)
+            {
+                Logger.Log("DNAI package has been created successfully.");
+            }
+            else
+            {
+                Logger.Log("Failed to create Unity Package.");
+            }
+        }
+    }
+}
--- DNAIPluginPublisher/ViewModel/MainViewModel.cs
+++ DNAIPluginPublisher/ViewModel/MainViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using GalaSoft.MvvmLight;
 using DNAIPluginPublisher.Model;
-using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.CommandWpf;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Collections.Generic;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace DNAIPluginPublisher.ViewModel
 {
@@ -13,6 +17,8 @@ namespace DNAIPluginPublisher.ViewModel
     public class MainViewModel : ViewModelBase
     {
         private readonly IDataService _dataService;
+
+        private ItemProvider _provider = new ItemProvider();
 
         /// <summary>
         /// The <see cref="WelcomeTitle" /> property's name.
@@ -37,6 +43,30 @@ namespace DNAIPluginPublisher.ViewModel
             }
         }
 
+        private ObservableCollection<Item> _items = new ObservableCollection<Item>();
+
+        public IReadOnlyList<Item> Items
+        {
+            get
+            {
+                return _provider.Items;
+            }
+        }
+
+        private string _userName = "";
+
+        public string UserName
+        {
+            get
+            {
+                return _userName;
+            }
+            set
+            {
+                Set(ref _userName, value);
+            }
+        }
+
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
@@ -54,30 +84,88 @@ namespace DNAIPluginPublisher.ViewModel
 
                     WelcomeTitle = item.Title;
                 });
+            Properties.Settings.Default.PropertyChanged += Default_PropertyChanged;
+            _provider.GetItems(Properties.Settings.Default.DirectoryPath);
             Logger.Log("DNAI Plugin Publisher version " + typeof(MainViewModel).Assembly.GetName().Version);
         }
 
-        ////public override void Cleanup()
-        ////{
-        ////    // Clean up if needed
+        private void Default_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(Properties.Settings.Default.DirectoryPath))
+            {
+                Logger.Log("Changing project location to path [" + Properties.Settings.Default.DirectoryPath + "]");
+                _provider.GetItems(Properties.Settings.Default.DirectoryPath);
+            }
+        }
 
-        ////    base.Cleanup();
-        ////}
+        public override void Cleanup()
+        {
+            // Clean up if needed
 
-        private RelayCommand _sendCommand;
+            base.Cleanup();
+            Properties.Settings.Default.PropertyChanged -= Default_PropertyChanged;
+            Properties.Settings.Default.Save();
+        }
+
+        private RelayCommand<PasswordBox> _sendCommand;
 
         /// <summary>
         /// Gets the SendCommand.
         /// </summary>
-        public RelayCommand SendCommand
+        public RelayCommand<PasswordBox> SendCommand
         {
             get
             {
                 return _sendCommand
-                    ?? (_sendCommand = new RelayCommand(
-                    () =>
+                    ?? (_sendCommand = new RelayCommand<PasswordBox>(
+                    (pbox) =>
                     {
                         Logger.Log("Starting...");
+                        var packer = new Packer();
+
+                        packer.Pack(_provider.Items);
+                    }, (e) => !string.IsNullOrEmpty(UserName)));
+            }
+        }
+
+        private RelayCommand _cancelCommand;
+
+        /// <summary>
+        /// Gets the CancelCommand.
+        /// </summary>
+        public RelayCommand CancelCommand
+        {
+            get
+            {
+                return _cancelCommand
+                    ?? (_cancelCommand = new RelayCommand(
+                    () =>
+                    {
+                        Logger.Log("Cancelling current task...");
+                    }));
+            }
+        }
+
+        private RelayCommand _pickFolderCommand;
+
+        /// <summary>
+        /// Gets the PickFolderCommand.
+        /// </summary>
+        public RelayCommand PickFolderCommand
+        {
+            get
+            {
+                return _pickFolderCommand
+                    ?? (_pickFolderCommand = new RelayCommand(
+                    () =>
+                    {
+                        var dialog = new CommonOpenFileDialog
+                        {
+                            IsFolderPicker = true
+                        };
+                        var res = dialog.ShowDialog();
+                        if (res == CommonFileDialogResult.Ok)
+                            Properties.Settings.Default.DirectoryPath = dialog.FileName;
                     }));
             }
         }
